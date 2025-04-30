3266d8047b0d0928c484a7a9f6312ed6a3423446
mccormickaustin <austinmccormick@mac.lan>
2025-04-30 19:47:55 -0400

    updating User_interface

--- User_interface.py
+++ User_interface.py
@@ -1,14 +1,14 @@
 import kivy 
 from kivy.app import App 
-from kivy.uix.label import label
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from Kivy.uix.textinput import TextInput 
+from kivy.uix.textinput import TextInput 
 
 class MyGrid(GridLayout):
     def __innit__(self, **kwargs):
         super(MyGrid, self).__innnit__(**kwargs)
         self.cols = 2
-        self.widget(Label(text="Name: ")
+        self.widget(Label(text="Name: "))
         self.name = TextInput(multiline=False)
         self.add_widget(self.name)
 
